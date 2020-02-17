032b414fa644e90839bae65c2ddd79daa6d7009d
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 14:54:38 +0530

    Create equi-tri.py

---
+++ codekata/absolutebeginner/equi-tri.py
@@ -0,0 +1,6 @@
+# The area of an equilateral triangle is ¼(√3a2) where "a" represents a side of the triangle. You are provided with the side "a". Find the area of the equilateral triangle.
+
+import math
+a = int(input())
+Area = (math.sqrt(3)/ 4)*(a * a)
+print("%.2f" %Area)
