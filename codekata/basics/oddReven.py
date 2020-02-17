c5c8c169f270ee7e2baf8c0d9c78a7e7abed417b
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 16:34:45 +0530

    Create oddReven.py

---
+++ codekata/basics/oddReven.py
@@ -0,0 +1,8 @@
+# Given 2 numbers N and M add both the numbers and check whether the sum is odd or even
+x, y = [int(x) for x in input("Enter two value: ").split()] 
+sum = 0
+sum = x + y
+if(sum % 2 == 0):
+  print("even")
+else:
+	print("odd")
