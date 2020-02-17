93768305de591f8ae29b816fe01aad80778c5f4a
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 12:12:41 +0530

    Update smallest.py

--- codekata/absolutebeginner/smallest.py
+++ codekata/absolutebeginner/smallest.py
@@ -1,5 +1,5 @@
  # You are provided with two numbers. Find and print the smaller number
- n, m = [int(i) for i in input().split()]
+ n, m = [float(i) for i in input().split()]
 if(n < m):
   print(n)
 else:
