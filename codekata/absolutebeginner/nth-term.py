e9362d7d2aefb2d8a7e20a431651607391cad4c8
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 14:46:43 +0530

    Create nth-term.py

---
+++ codekata/absolutebeginner/nth-term.py
@@ -0,0 +1,10 @@
+# You are provided with a number "N", Find the Nth term of the series: 1, 4, 9, 25, 36, 49, 64, 81
+n = float(input())
+if(n > 0):
+   mul = n * n
+   print(mul)
+elif(n == 0):
+   print("0")
+else:
+   print("Error")
+ 
