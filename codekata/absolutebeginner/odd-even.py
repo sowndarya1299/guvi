9234aac6e9726ac13a42713de676d17838fd0134
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 14:57:11 +0530

    Create odd-even.py

---
+++ codekata/absolutebeginner/odd-even.py
@@ -0,0 +1,9 @@
+You are provided with a number check whether its odd or even
+num = int(input())        
+round(num)  
+if(num == 0):
+  print("Zero")
+if(num % 2 == 0):
+  print("Even")
+else:
+  print("Odd")
