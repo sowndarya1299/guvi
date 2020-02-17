29c2a5fe4e0f128dd5264e2d9980040549a840b3
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 14:30:32 +0530

    Create fahrenheit.py

---
+++ codekata/absolutebeginner/fahrenheit.py
@@ -0,0 +1,4 @@
+# You are given with a number A i.e. the temperature in Celcius. Write a program to convert this into Fahrenheit. 
+celsius = float(input()) 
+fahrenheit = (celsius * 1.8) + 32
+print("%0.2f" %fahrenheit)
