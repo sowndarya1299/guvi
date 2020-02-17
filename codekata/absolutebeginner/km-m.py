ae72ef22e4a05994173aa71760287635cf9a5580
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-17 14:19:07 +0530

    Create km-m.py

---
+++ codekata/absolutebeginner/km-m.py
@@ -0,0 +1,6 @@
+You are given a number A in Kilometers. Convert this into B: Meters and C: Centi-Metres.
+a = int(input())
+meter = a * 1000
+centimeter = a * 100000
+print(meter)
+print(centimeter)
