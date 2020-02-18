cf97433b9695c96518db2dffc3722845c1eb793e
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-18 22:43:21 +0530

    Create cuboid.py

---
+++ codekata/mathematics/cuboid.py
@@ -0,0 +1,9 @@
+# to calculate the total surface area and volume of cuboid
+import math
+n,m = int(input().split()
+mul = n * m
+z = math.sqrt(mul)  
+if(z == n) or (z == m):
+    print("yes")
+else:
+    print("no")
