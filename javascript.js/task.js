861cc706522cb1727c4911504a0429d6f4b7e435
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-02-26 12:17:22 +0530

    Update task.js

--- javascript.js/task.js
+++ javascript.js/task.js
@@ -20,11 +20,11 @@ const finditem=a.find((item)=>{
 console.log(finditem)
 
 #filter method to print the capital less then 5
-const.cap=a.filter((item)=>{
+const cap=a.filter((item)=>{
   return(iten.capital).length<=5})
 console.log(finditem)
 
 #map function
-const.cap=a.map((item)=>{
+const cap=a.map((item)=>{
   return(iten.name).toUpperCase()})
 console.log(cap)
