d3aeec4f183fb0c7021de3208a5d438afda8ee0d
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-03-02 12:58:13 +0530

    Create swap.js

---
+++ javascript.js/swap.js
@@ -0,0 +1,13 @@
+# swap string
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+inp.on("line", (data) => {
+  var a = data.split(" ");
+  var t;
+  t=a[0];
+  a[0]=a[1];
+  a[1]=t;
+	console.log(a.join(' '))
+});
