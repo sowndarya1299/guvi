b6f40371a81318c504f4343d4390c76a4aeb52fc
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-03-05 13:02:56 +0530

    Update si.js

--- javascript.js/si.js
+++ javascript.js/si.js
@@ -1,12 +1,13 @@
+
+
 const readline = require('readline');
-const inp = readline.createInterface({
-  input: process.stdin
-});
+const inp = readline.createInterface({input: process.stdin});
 inp.on("line", (data) => {
-  var a=data.split(" ");
-  var p=a[0];
-  var r=a[1];
-  var t=a[2];
-  var si=(parseInt(p)*parseInt(t)*parseInt(r))/100;
-  console.log(parseInt(si).toFixed(2));
+var a=[];
+ a=data.split(" ");
+ var p=a[0];
+ var T=a[1];
+ var r=a[2];
+ var SI = (p*T*r)/100;
+ console.log(SI.toFixed(2));
 });
