5f1237e8ae324913fd2489b9b250ded0ee65e8e3
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-03-05 12:54:42 +0530

    Update mulof2.js

--- javascript.js/mulof2.js
+++ javascript.js/mulof2.js
@@ -1,10 +1,11 @@
+
 const readline = require('readline');
-const inp = readline.createInterface({
-  input: process.stdin
-});
+const inp = readline.createInterface({input: process.stdin});
 inp.on("line", (data) => {
-  var a =[]
-  for(var i=1;i<=3;i++)
-   a = data*i
-   console.log(a)
+var a=[];
+for(i=1;i<=3;i++)
+{
+a.push(data*i)
+}
+console.log(a.join(" "));
 });
