dba01dcb35971ec221df2b0d79056110c6d762eb
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-03-10 12:44:37 +0530

    Update gcd.js

--- javascript.js/basics/gcd.js
+++ javascript.js/basics/gcd.js
@@ -1,4 +1,24 @@
 
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+var a=[]
+a=data.split(' ');
+var x=a[0]
+var y=a[1]
+x = Math.abs(x);
+y = Math.abs(y);
+while(y) {
+var t = y;
+y = x % y;
+x = t;
+}
+if(num>0){
+console.log(t);}
+else{
+console.log('-1');
+}
+});
 const readline = require('readline');
 const inp = readline.createInterface({
   input: process.stdin
