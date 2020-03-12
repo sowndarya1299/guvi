64e3fd9dcc0b70687a4e2078245e6a3ac34b3357
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-03-12 17:15:25 +0530

    Update palindrome.js

--- javascript.js/string/palindrome.js
+++ javascript.js/string/palindrome.js
@@ -1,16 +1,14 @@
+
 const readline = require('readline');
-const inp = readline.createInterface({
-  input: process.stdin
-});
+const inp = readline.createInterface({input: process.stdin});
 inp.on("line", (data) => {
-  var t=data;
-  var a=[];
-  a=data.split("");
-  var b=a.reverse();
-  var c=b.join('')
-  console.log(c);
-  if(t==c)
-	console.log("yes");
-  else
-    console.log("no");
-});
+var t=data;
+a=[];
+a=data.split("");
+var m=a.reverse();
+var n=m.join("");
+if(t==n)
+console.log("yes")
+else
+console.log("no")
+})
