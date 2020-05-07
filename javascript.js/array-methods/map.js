721969b52d0c822aed5115447991f18f0028ae1c
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-05-07 17:45:26 +0530

    Update map.js

--- javascript.js/array-methods/map.js
+++ javascript.js/array-methods/map.js
@@ -1,3 +1,11 @@
-var numbers = [4, 9, 16, 25];
-var x = numbers.map(Math.sqrt)
-console.log(x);
+const function1 = (a, callback) => {
+  let b=[];
+  for(var i=0;i<a.length;i++){
+    b.push(callback(a[i]));
+  }
+  return b
+}
+const mapfun =(value)=>Math.sqrt(value);
+fun1([2,4,9], mapfun) 
+
+//output:[1.4142135623730951, 2, 3]
