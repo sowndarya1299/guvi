7a33d679cb3a12575d0807c1e212bb749e28a411
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-05-07 17:37:16 +0530

    Update filter.js

--- javascript.js/array-methods/filter.js
+++ javascript.js/array-methods/filter.js
@@ -1,9 +1,14 @@
-var ages = [32, 33, 16, 40];
-function check(age) {
-  return age >= 18;
+const function1 = (a, callback) => {
+  let b=[];
+  for(var i=0;i<a.length;i++){
+    if(callback(a[i]))
+      {
+        b.push(a[i]);
+      }
+  }
+  return b
 }
+const filfun =(value)=>value%2==0
+function1([1,2,3,4,5,6,7], filfun) 
 
-function fil() {
-  var a= age.filter(check);
-  console.log(a);
-}
+//output:[2,4,6]
