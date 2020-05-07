f6c79b05e96bfeaa6604ef6798d2da0de5567710
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-05-07 17:28:16 +0530

    Update concat.js

--- javascript.js/array-methods/concat.js
+++ javascript.js/array-methods/concat.js
@@ -1,6 +1,9 @@
-function con() {
-  var a = "Hello ";
-  var b = "welcome";
-  var res = a.concat(b);
-  console.log(res);
+const function1 = (a1,a2) => {
+  let b=a1;
+  for(var i=0;i<a2.length;i++){
+    b.push(a2[i]);
+  }
+  return a
 }
+function1([234,7,41,31],[79,431,76,91])
+//output:234,7,41,31,79,431,76,91
