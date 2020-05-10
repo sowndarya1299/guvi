4dc7bb0872a20131b76feb7cd84f7660b0af0e11
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-05-10 16:04:20 +0530

    Create reverse.js

---
+++ javascript.js/zoho/reverse.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const n = [];
+inp.on("line", (data) => {
+n.push(data);
+});
+inp.on("close", () => {
+	var a=n[0];
+    var b=n[1];
+    console.log(b.split(" ").reverse().join(' '));
+    
+});
+
+
