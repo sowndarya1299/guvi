016e5d19c5004e9c11c3caa50c9faf0cb89e989d
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-06-03 07:45:18 +0530

    Update 1.3.js

--- day2(2-6-20)/1.3.js
+++ day2(2-6-20)/1.3.js
@@ -14,4 +14,4 @@ var request=new XMLHttpRequest()
 request.open("GET","https://restcountries.eu/rest/v2/all",true)//link is to get the rest countries
 request.onload=function(){
     var jsonData=JSON.parse(this.response)
-    for(var i in jsonData){console.log(jsonData[i].name,":",jsonData[i].alpha3Code,"    population :",jsonData[i].population)}};request.send();//to print the country name along with flag
+    for(var i in jsonData){console.log(jsonData[i].name,":",jsonData[i].alpha3Code,"    population :",jsonData[i].population)}};request.send();//to print the country name along with alpha3code and its population
