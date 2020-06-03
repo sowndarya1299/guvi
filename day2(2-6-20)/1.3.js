3778cf8bf6b9b87151d732be5fe16ac587299535
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-06-03 07:42:38 +0530

    Create 1.3.js

---
+++ day2(2-6-20)/1.3.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>country name with alpha3code and population</title>
+    </head>
+    <body>
+        
+    <script src="api.js">
+        </script>
+    </body>
+    </html>
+//script tag
+var request=new XMLHttpRequest()
+request.open("GET","https://restcountries.eu/rest/v2/all",true)//link is to get the rest countries
+request.onload=function(){
+    var jsonData=JSON.parse(this.response)
+    for(var i in jsonData){console.log(jsonData[i].name,":",jsonData[i].alpha3Code,"    population :",jsonData[i].population)}};request.send();//to print the country name along with flag
