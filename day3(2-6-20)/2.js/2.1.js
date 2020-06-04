641aca85408bf4fa22bf1c16bfccf7f31cd7f964
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-06-04 06:37:17 +0530

    Create 2.1.js

---
+++ day3(2-6-20)/2.js/2.1.js
@@ -0,0 +1,11 @@
+var arr=[];
+var odd=function(odd)
+{
+    for(var i=0;i<=odd;i++)//iteration to generate a numbers form 0 to 0dd value
+    {
+        if(i%2!=0){
+        
+            arr.push(i); }}//new array is created to push the odd numbers
+    console.log(arr)
+}
+odd(15);//functuion is called
