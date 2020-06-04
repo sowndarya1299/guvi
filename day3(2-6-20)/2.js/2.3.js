1b68ebffca804557cfefc55b9a714013935c5644
sowndarya1299 <61134852+sowndarya1299@users.noreply.github.com>
2020-06-04 07:12:41 +0530

    Create 2.3.js

---
+++ day3(2-6-20)/2.js/2.3.js
@@ -0,0 +1,9 @@
+//2.3. Sum of all numbers in an array
+var arr1=[1,2,3,4];
+var num=function(a)//function is created
+{
+var add=0;
+for(i=0;i<arr1.length;i++){add=add+arr1[i];}
+console.log(add);
+}
+num(arr);//function is called
